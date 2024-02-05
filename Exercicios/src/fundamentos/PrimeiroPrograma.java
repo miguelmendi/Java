ef14878002698b8a9714047338870d1d277da563
Miguel <miguelmds925@gmail.com>
2024-02-05 16:09:43 -0300

    Vendo sobre como fazer os comentários no Java

--- Exercicios/src/fundamentos/PrimeiroPrograma.java
+++ Exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -2,6 +2,8 @@ package fundamentos;
 
 public class PrimeiroPrograma {
 	public static void main(String[] args) {
+		
+		// setença de código termina com ;
 		System.out.println("Primeiro prograna Parte #01");
 		System.out.println("Primeiro prograna Parte #02");
 		
