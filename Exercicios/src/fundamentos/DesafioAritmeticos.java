386d8a1cf196a9db4406c36ec3a766153e68fc7c
Miguel <miguelmds925@gmail.com>
2024-02-14 17:15:00 -0300

    Começando o desafio aritmético

---
+++ Exercicios/src/fundamentos/DesafioAritmeticos.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class DesafioAritmeticos {
+	public static void main(String[] args) {
+		int ab = 3 * 4 - 10 ;
+		int bb= (int) Math.pow(ab, 3);
+		
+		System.out.println(bb);
+	}
+}
