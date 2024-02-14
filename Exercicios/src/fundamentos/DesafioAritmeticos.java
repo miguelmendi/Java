efaa84ef25192b25eca76f4ba5aa2d1763702e1d
Miguel <miguelmds925@gmail.com>
2024-02-14 17:56:49 -0300

    Resolvendo o desafio

--- Exercicios/src/fundamentos/DesafioAritmeticos.java
+++ Exercicios/src/fundamentos/DesafioAritmeticos.java
@@ -6,5 +6,25 @@ public class DesafioAritmeticos {
 		int bb= (int) Math.pow(ab, 3);
 		
 		System.out.println(bb);
+		
+		double a = 6 *(3 +2);
+		double aPotentiation = Math.pow(a, 2);
+		System.out.println(aPotentiation);
+		
+		double b = 3 * 2;
+		double c = aPotentiation / b;
+		
+		double d = (1 - 5) * (2 - 7);
+		double e = d /2;
+		double ePotentiation = Math.pow(e, 2);
+		double f = c - ePotentiation;
+		
+		double fPotentiation = Math.pow(f, 3);
+		System.out.println(fPotentiation);
+		double g = 10;
+		double gPotentiation = Math.pow(g, 3);
+		
+		double result = fPotentiation / gPotentiation;
+		System.out.println(result);
 	}
 }
