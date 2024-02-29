62b7897170a8b6abf3ccf1b7e7b0e7d63dbc417d
Miguel <miguelmds925@gmail.com>
2024-02-29 16:16:49 -0300

    Resolvendo o quinto exercício

---
+++ Exercicios/src/controle/Exercicio5.java
@@ -0,0 +1,31 @@
+package controle;
+
+import java.util.Scanner;
+
+public class Exercicio5 {
+	public static void main(String[] args) {
+		
+			int contadorDeDivisores = 0;
+			Scanner scanner = new Scanner(System.in);
+
+			System.out.println("\nDigite um numero para verificar se é primo:");
+			int numero = scanner.nextInt();
+
+			for (int i = 2; i < numero; i++) {
+				if (numero % i == 0) {
+					contadorDeDivisores++;
+				}
+			}
+
+			switch(contadorDeDivisores) {
+			case 0:
+				System.out.println("\nO numero " + numero + " é primo.");
+				break;
+			 default:
+				System.out.println("\nO numero " + numero + " não é primo.");
+			}
+
+			scanner.close();
+
+		}
+}
