f65c78e598ef2b3530889d0de18803b352a27558
Miguel <miguelmds925@gmail.com>
2024-03-05 18:20:28 -0300

    Vendo a criação de métodos

--- Exercicios/src/classe/Produto.java
+++ Exercicios/src/classe/Produto.java
@@ -4,4 +4,8 @@ public class Produto {
 	String nome;
 	double preco;
 	double desconto;
+	
+	double precoComDesconto(double descontoDoGerente) {
+		return preco * (1 - desconto + descontoDoGerente);
+	}
 }
--- Exercicios/src/classe/ProdutoTeste.java
+++ Exercicios/src/classe/ProdutoTeste.java
@@ -16,8 +16,8 @@ public class ProdutoTeste {
 		System.out.println(p2.nome);
 		System.out.println(p1.nome);
 		
-		double precoFinal1 = p1.preco * (1 - p1.desconto);
-		double precoFinal2 = p2.preco * (1 - p2.desconto);
+		double precoFinal1 = p1.precoComDesconto(0);
+		double precoFinal2 = p2.precoComDesconto(0.1);
 		
 		System.out.println(precoFinal1);
 		System.out.println(precoFinal2);
