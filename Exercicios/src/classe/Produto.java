a3a6983d648510b504e30da61b2d3a73f47f48ca
Miguel <miguelmds925@gmail.com>
2024-03-07 19:44:17 -0300

    Resolvendo o desafio

--- Exercicios/src/classe/Produto.java
+++ Exercicios/src/classe/Produto.java
@@ -3,18 +3,20 @@ package classe;
 public class Produto {
 	String nome;
 	double preco;
-	double desconto;
-	
-	
-	
-	Produto(String nomeInicial, double precoInicial, double descontoInicial) {
-		nome = nomeInicial;
-		preco = precoInicial;
-		desconto = descontoInicial;
-	}
+	static double desconto = 0.25;
 	
 	Produto() {
 		
+	}
+	
+	Produto(String nomeInicial, double precoInicial) {
+		nome = nomeInicial;
+		preco = precoInicial;
+		
+	}
+	
+	double precoComDesconto() {
+		return preco * (1 - desconto);
 	}
 	
 	double precoComDesconto(double descontoDoGerente) {
