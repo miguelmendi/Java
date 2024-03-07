ed12218278df3c8907f888ad1efa033a8606f3b9
Miguel <miguelmds925@gmail.com>
2024-03-07 18:25:48 -0300

    Resolvendo o desafio usando construtores

--- Exercicios/src/classe/DataTeste.java
+++ Exercicios/src/classe/DataTeste.java
@@ -4,17 +4,10 @@ public class DataTeste {
 	public static void main(String[] args) {
 		Data data1 = new Data();
 		
-		data1.dia = 12;
-		data1.mes = 8;
-		data1.ano = 2015;
-		
 		System.out.println(data1.obterDataFormatada());
 		
-		Data data2 = new Data();
+		Data data2 = new Data(31, 12, 2020);
 		
-		data2.dia = 15;
-		data2.mes = 4;
-		data2.ano = 2016;
 		
 		System.out.println(data2.obterDataFormatada() );
 	}
