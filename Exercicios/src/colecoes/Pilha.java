9168c0dafe0eb35f81d83be23fec61164e8c7647
Miguel <miguelmds925@gmail.com>
2024-03-18 15:37:51 -0300

    Aprendendo sobre o tipo de estrutura pilha

---
+++ Exercicios/src/colecoes/Pilha.java
@@ -0,0 +1,22 @@
+package colecoes;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Pilha {
+	public static void main(String[] args) {
+		Deque<String> livros = new ArrayDeque<String>();
+		
+		livros.add("O pequeno príncipe");
+		livros.push("Don quixote");
+		livros.push("O hobbit");
+		
+		System.out.println(livros.peek());
+		System.out.println(livros.element());
+		
+		System.out.println(livros.poll());
+		System.out.println(livros.poll());
+		System.out.println(livros.poll());
+		System.out.println(livros.poll());
+	}
+}
