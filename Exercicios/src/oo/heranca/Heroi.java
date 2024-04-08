41df3437d8f4b071daec0f5df4471e4a1cd2f9d0
Miguel <miguelmds925@gmail.com>
2024-04-08 16:05:04 -0300

    Vendo as maneiras corretas de chamar os construtores

--- Exercicios/src/oo/heranca/Heroi.java
+++ Exercicios/src/oo/heranca/Heroi.java
@@ -2,7 +2,11 @@ package oo.heranca;
 
 public class Heroi extends Jogador {
 	
-boolean atacar(Jogador oponente) {
+	Heroi(int x, int y) {
+		super(0, 0);
+	}
+	
+	boolean atacar(Jogador oponente) {
 		boolean ataque1 = super.atacar(oponente);
 		boolean ataque2 = super.atacar(oponente);
 		boolean ataque3 = super.atacar(oponente);
