04a8f08c5759a650228e44945130d0bf8fa139d9
Miguel <miguelmds925@gmail.com>
2024-04-11 19:29:50 -0300

    Aprendendo sobre as classes abstratas

---
+++ Exercicios/src/oo/abstrato/Cachorro.java
@@ -0,0 +1,13 @@
+package oo.abstrato;
+
+public class Cachorro extends Mamifero {
+	//@Override
+	//public String mover() {
+		// TODO Auto-generated method stub
+		//return "Usando as patas";
+	//}
+	
+	public String mamar() {
+		return "Usando leite";
+	}
+}
---
+++ Exercicios/src/oo/abstrato/Mamifero.java
@@ -0,0 +1,12 @@
+package oo.abstrato;
+
+public abstract class Mamifero extends Animal {
+
+	public abstract String mamar();
+	
+	@Override
+	public final String mover() {
+		
+		return "Saindo do lugar";
+	}
+}
