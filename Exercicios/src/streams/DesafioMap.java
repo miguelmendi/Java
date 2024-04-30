f4bccebfe86fc0a52cc7ad8d06e4f331dc88b31d
Miguel <miguelmds925@gmail.com>
2024-04-30 19:22:09 -0300

    Começando o desafio do map

---
+++ Exercicios/src/streams/DesafioMap.java
@@ -0,0 +1,10 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DesafioMap {
+	public static void main(String[] args) {
+		List<Integer> nums =  Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+	}
+}
